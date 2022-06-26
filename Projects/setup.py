d65547ab022bc7dbda0372c4f7b73ed00dbc6341
arshilvhora27 <95517536+arshilvhora27@users.noreply.github.com>
2022-06-26 17:50:20 -0500

    updated

---
+++ Projects/setup.py
@@ -0,0 +1,34 @@
+import imp
+from webbrowser import get
+from setuptools import setup, find_packages
+from typing import List
+
+#declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.4"
+AUTHOR="Arshil Vhora"
+DESCRIPTION="This is my first machine learning  project with deployments"
+REQUIREMENT_FILE_NAME='requirements.txt'
+
+def get_requirements_list()->List[str]:
+    """"
+    Description : This function is going to return list of requirement
+    mentioned in requirements.txt"
+
+    return This function is going to return a list which contain name of libraries mentioned in requirements.txt
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove("-e .")
+        
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=find_packages(),
+install_requires=get_requirements_list()
+
+)
+
